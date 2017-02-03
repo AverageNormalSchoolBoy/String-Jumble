6fe1ebef0aa3e2be89fb2d0afde11242f27a62b5
AverageNormalSchoolBoy <brendandufty@hanovernorwichschools.org>
2017-02-03 14:40:34 -0500

    Updated from Brython Server: 2/3/2017 2:40:33 PM

--- stringjumble.py
+++ stringjumble.py
@@ -25,3 +25,8 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
+
+sent= input("Please enter a string of text (the bigger the better): ")
+nd = sent.split()
+nd1 = zip(nd,list(range(1,len(nd)+1)))
+print(list(nd1))
