4c959071be718085a52ca2833a3fcd12875de5ec
AverageNormalSchoolBoy <brendandufty@hanovernorwichschools.org>
2017-02-03 19:03:30 -0500

    Updated from Brython Server: 2/3/2017 7:03:28 PM

--- stringjumble.py
+++ stringjumble.py
@@ -27,7 +27,7 @@ erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
 
 sent= input("Please enter a string of text (the bigger the better): ")
-print('You entered "{0}".Now jumble it: ' .format(sent))
+print('You entered "{0}". Now jumble it: ' .format(sent))
 
 st = (list(sent))
 st.reverse()
